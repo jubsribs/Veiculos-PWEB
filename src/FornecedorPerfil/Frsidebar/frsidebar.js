3e798e89b61a5f264b6f0d3a538a28c407afd74e
Juliana Ribeiro <57453692+jubsribs@users.noreply.github.com>
2021-12-15 11:06:28 -0300

    Add files via upload

---
+++ src/FornecedorPerfil/Frsidebar/frsidebar.js
@@ -0,0 +1,70 @@
+const frsidebar = ({frsidebarOpen} ,frcloseSidebar) => {
+    return(
+        <div className= {frsidebarOpen ? "frsidebar-responsive": "" }id="frsidebar">
+            <div className="frsidebar__title">
+                <div className = "frsidebar_img">
+                    <h1> Locação Veículos </h1>
+                </div>
+
+                <i 
+                onClick ={() => frcloseSidebar()}
+                className ="fa fa-times"
+                id = "frsidebarIcon"
+                aria-hidden ="true"
+                ></i>
+            </div>
+            
+    
+            <div className ="frsidebar_menu">
+                <div className = "frsidebar__link active_menu_link">
+                    <i className = "fa fa-minus-square">  
+                    </i>
+                        <a href = "#">
+                            Home
+                        </a>
+                </div>
+               
+
+                <h2>
+                Cadastrar Veiculos
+                </h2>
+                <div className = "frsidebar__link">
+                <i className = "fa fa-tachometer">  
+                    </i>
+                        <a href = "#">
+                            Cadastrar
+                        </a>
+                </div>
+
+                <h2>
+                     Gerenciar Veiculos
+                </h2>
+                <div className = "frsidebar__link">
+                <i className = "fa fa-archive">  
+                    </i>
+                        <a href = "#">
+                            Gerenciar
+                        </a>
+                </div>
+
+                <div className = "frsidebar__link">
+                <i className = "fa fa-file-text">  
+                    </i>
+                        <a href = "#">
+                            Política de Privacidade
+                        </a>
+                </div>
+
+                <div className = "frsidebar__log out">
+                <i className = "fa fa-power-off">  
+                    </i>
+                        <a href = "#">
+                            Log out
+                        </a>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default frsidebar;
