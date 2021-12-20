7b9bb16a8b67fad36d045c6dfd3342950590dadd
Juliana Ribeiro <57453692+jubsribs@users.noreply.github.com>
2021-12-20 14:36:18 -0300

    Add files via upload

---
+++ src/PosLogin/plogin.js
@@ -0,0 +1,21 @@
+import './plogin.css';
+
+function plogin(){
+    return (
+<div class="field">
+  <span class="select">
+    <select>
+      <option selected>Cliente</option>
+      <option>Funcionario</option>
+      <option>Fornecedor</option>
+    </select>
+  </span>
+<button type="submit">
+          Cadastrar
+      </button>
+</div>
+
+
+    )
+};
+export default plogin;
